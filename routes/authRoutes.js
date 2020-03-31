231b9556d148e9f6adb3afead316ea74a4d622e5
Shayan Raouf <rshayan@amazon.com>
2020-03-30 23:29:13 -0700

    finished auth flow

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
-module.exports = (app) => {
+
+module.exports = app => {
     app.get(
         '/auth/google', 
         passport.authenticate('google', {
@@ -7,5 +8,17 @@ module.exports = (app) => {
         })
     );
     
-    app.get('/auth/google/callback', passport.authenticate('google'));
+    app.get(
+        '/auth/google/callback', 
+        passport.authenticate('google')
+    );
+
+    app.get('/api/logout', (req, res) => {
+        req.logout();
+        res.redirect('/');
+    });
+
+    app.get('/api/current_user', (req, res) => {
+        res.send(req.user);
+    });
 };
